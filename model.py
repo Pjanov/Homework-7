1f378d7a028c4d3136c658147c9c1a69d57366e4
Андрей <pjanov84@mail.ru>
2022-11-14 17:06:59 +0300

    добавил функции в model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+def write_file(data: str, file_name: str) -> str:
+    '''
+    Открывает указанный файл для записи не затирая предыдущие данные
+    :param data: принемает строку с данными
+    :param file_name: принемает имя и путь к файлу
+    :return: записывает данные в файл
+    '''
+    with open(file_name, 'a', encoding='utf-8') as file:
+        return file.writelines(data)
+
+
+def read_data(file_name: str) -> str:
+    '''
+    Считывает все данные из файла
+    :param file_name: принемает имя и путь к файлу
+    :return: строки с данными
+    '''
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+        return data
+
+
+if __name__ == '__main__':
+    pass
